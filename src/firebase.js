3064a5e129a4ddbbe5163b8a3e180d901c4f6120
sayn006 <sayn.punithan@gmail.com>
2020-11-06 11:35:42 +0100

    evolution of this app
    last update

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBa_Ifraq5lSZk9UFcbMncTs8BDBpAH8vM",
+    authDomain: "imessage-clone-yt-ee002.firebaseapp.com",
+    databaseURL: "https://imessage-clone-yt-ee002.firebaseio.com",
+    projectId: "imessage-clone-yt-ee002",
+    storageBucket: "imessage-clone-yt-ee002.appspot.com",
+    messagingSenderId: "554340052182",
+    appId: "1:554340052182:web:547e2bcecea0a4a092ed77",
+    measurementId: "G-TF88X0V720"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
