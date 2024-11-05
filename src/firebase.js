3cf114ed081f06ab2bfd9154dc77dd4af637353b
sayn006 <sayn.punithan@gmail.com>
2024-11-05 17:10:00 +0100

    Update firebase.js
    remove api key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBa_Ifraq5lSZk9UFcbMncTs8BDBpAH8vM",
+    apiKey: "******************************",
     authDomain: "imessage-clone-yt-ee002.firebaseapp.com",
     databaseURL: "https://imessage-clone-yt-ee002.firebaseio.com",
     projectId: "imessage-clone-yt-ee002",
     storageBucket: "imessage-clone-yt-ee002.appspot.com",
-    messagingSenderId: "554340052182",
+    messagingSenderId: "**********************",
     appId: "1:554340052182:web:547e2bcecea0a4a092ed77",
     measurementId: "G-TF88X0V720"
   };
